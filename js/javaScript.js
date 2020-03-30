2462345823571c414cf12258a3cd80c05f12d2bf
LeroyAndrade <30168@ma-web.nl>
2020-03-30 03:59:05 +0200

    Werkend menu
    Todo:
    Unicode klikbaar maken met .js en de rest van de webpagina met CSS later transitioneren

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,1 +1,7 @@
-window.alert("geslaagd");
+/* querySelector after a specific element: https://stackoverflow.com/questions/20419082/queryselector-after-a-specific-element
+zodat het menu verschijnt en verdwijnt wanneer er op wordt geklikt*/
+
+/*document.getElementById('toggleLabel')addEventListener('click', function(){
+  window.alert('click');
+});
+*/
